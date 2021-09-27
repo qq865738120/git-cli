da2474def02c60e5b6a41accc855e29318a90611
zwj <zwj865738120@163.com>
2021-09-27 16:36:57 +0800

    feat:acp命令开发

--- source/ui/AcpView.tsx
+++ source/ui/AcpView.tsx
@@ -1,16 +1,39 @@
-import React, { FC, useState } from "react"
-import { Box, Text } from "ink"
+import React, { FC, useEffect, useState } from "react"
+import { Box, Newline, Text, useApp } from "ink"
 import TextInput from "ink-text-input"
+import ProgressBar from "ink-progress-bar"
 import shell from "shelljs"
+// import logSymbols from "log-symbols"
 
 const AcpView: FC = () => {
 	const [submitMessage, setSubmitMessage] = useState("")
+	const [tasks, setTasks] = useState([] as ((params?: any) => string)[])
+	const [taskDone, setTaskDone] = useState([] as ((params?: any) => string)[])
+	const [progressText, setProgressText] = useState("")
+	const { exit } = useApp()
+
+	useEffect(() => {
+		setTasks([
+			() => "git add .",
+			() => `git commit -m "${submitMessage}"`,
+			() => "git push",
+		])
+	}, [])
 
 	const onInputSubmit = () => {
+		tasks.map(task => {
+			const result = shell.exec(task(), { silent: true })
+			if (result.code === 0) {
+				setProgressText(task().substring(0, 8))
+				setTaskDone(taskDone.concat([task]))
+			} else {
+				exit(new Error(task() + "命令运行失败"))
+			}
+		})
 		const result = shell.exec("git add .", { silent: true })
-    console.log('result', result);
+		console.log("result", result)
 		shell.exec(`git commit -m "${submitMessage}"`, { silent: true })
-    shell.exec("git pull", { silent: true })
+		// shell.exec("git pull", { silent: true })
 		shell.exec("git push", { silent: true })
 	}
 
@@ -21,6 +44,12 @@ const AcpView: FC = () => {
 				value={submitMessage}
 				onChange={setSubmitMessage}
 				onSubmit={onInputSubmit}
+			/>
+			<Newline />
+			<Text>{progressText}</Text>
+			<ProgressBar
+				left={progressText.length}
+				percent={taskDone.length / tasks.length}
 			/>
 		</Box>
 	)
